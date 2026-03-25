fd5686f2782ba7bf8545a1e91d80f7dcaa6b598a
sharmaash730-ops <sharmaash730@gmail.com>
2026-03-25 09:38:32 -0700

    Add files via upload

---
+++ handling loading state while fetching data/handling loading state while fetching data/todos/page.js
@@ -0,0 +1,29 @@
+const todos = async () => {
+    const slowResponse = await fetch("https://procodrr.vercel.app/?sleep=2000")
+    const data = await slowResponse.json();
+    console.log(data);
+
+    const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos?_limit=5"
+    );
+    const todos = await response.json();
+
+    return (
+        <>
+            <h1>Todos</h1>
+            <div className="todos-container">
+                {
+                    todos.map(({ id, title, completed }) => (
+                        <div className="todo-item" key={id}>
+                            <input type="checkbox" checked={completed} readOnly />
+
+                            <p>{title}</p>
+                        </div>
+                    ))
+                }
+            </div>
+
+        </>
+    );
+};
+export default todos;
