9ec03e18ed5a950155e3a7f0fb0c02ca968a6e2d
sharmaash730-ops <sharmaash730@gmail.com>
2026-03-25 09:36:16 -0700

    Add files via upload

---
+++ data fetching in server components/data fetching in server components/page.js
@@ -0,0 +1,11 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Home page</h1>
+
+            <p>welcome to home website</p>
+        </div>
+    );
+};
+export default Home;
---
+++ data fetching in server components/data fetching in server components/todos/page.js
@@ -0,0 +1,27 @@
+
+const posts = async () => {
+
+    const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos?_limit=5"
+    );
+    const todos = await response.json();
+
+    return (
+        <>
+            <h1>todos</h1>
+            <div className="todos-container">
+                {
+                    todos.map(({ id, title, completed }) => (
+                        <div className="todo-item" key={id}>
+                            <input type="checkbox" checked={completed} readOnly />
+
+                            <p>{title}</p>
+                        </div>
+                    ))
+                }
+            </div>
+
+        </>
+    );
+};
+export default posts;
