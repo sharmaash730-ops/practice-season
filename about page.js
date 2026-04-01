4e21124e7afb04e0f3c7df41dabb0702a02d83e4
sharmaash730-ops <sharmaash730@gmail.com>
2026-04-01 08:22:35 -0700

    Delete about page.js

--- about page.js
+++
@@ -1,9 +0,0 @@
-import Link from "next/Link";
-export default function about() {
-    return (
-        <>
-            <h1> about page</h1>
-            <Link from href="/">hoome</Link>
-        </>
-    )
-};
