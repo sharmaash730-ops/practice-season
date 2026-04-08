b25b3bff486bf88a9b4d1018d15f1874a448de56
sharmaash730-ops <sharmaash730@gmail.com>
2026-04-08 01:25:15 -0700

    Delete routing with express.js directory

--- routing with express.js/db/connection.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1/hotel')
-    .then(() => console.log('***connected***'))
-    .catch(error => handleError(error.message));
-
--- routing with express.js/models/user.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const userSchema = new mongoose.Schema({
-    id: Number,
-    name: String,
-    email: String,
-    password: String
-});
-module.exports = mongoose.model('ashish2', userSchema);
