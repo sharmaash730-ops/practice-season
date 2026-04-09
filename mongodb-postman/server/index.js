69d5b47579ff0b5bd345e848d79b099c763a080d
sharmaash730-ops <sharmaash730@gmail.com>
2026-04-09 09:00:01 -0700

    Update index.js

--- mongodb-postman/server/index.js
+++ mongodb-postman/server/index.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+//change dns
+const dns=require('dns');
+dns.setServers(["1.1.1.1","8.8.8.8"])
+
+//database connection
 require('./db/connection');
 const Users = require('./Models/User');
 
+//post function
 app.post('/', async (req, res) => {
     let user = new Users(req.body);
     let result = await user.save();
