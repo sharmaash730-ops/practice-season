8087ccb71efb7116a0dfc36e283dc10ebfc2f558
sharmaash730-ops <sharmaash730@gmail.com>
2026-04-21 22:37:56 -0700

    Add files via upload

---
+++ edit todo/app/todos/[id]/route.js
@@ -0,0 +1,35 @@
+import {writeFile} from "node:fs/promises"
+import todos from "../../../todos.json";
+
+export async function GET(_,{params}){
+    const { id}=await params;
+    const todo=todos.find((todos)=>id===todo.id.toString());
+
+    if (!todo){
+        return Response.json(
+            {error:"todo not found"},
+            {status:404,}
+        );
+    }
+    return Response.json(todo);
+}
+export async function PUT(request,{params}){
+   const editTodoData=await request.json();
+   const {id}=await params;
+   const todoIndex=todos.findIndex((todo)=>id === todo.id);
+   const todo=todos[todoIndex];
+
+   if(editTodoData.id){
+    return Response.json(
+        {error:"changing id not allowed"},
+        {
+            status:403,
+        }
+    )
+   };
+   const editedTodo={...todo,...editTodoData};
+todos[todoIndex]=editedTodo;
+
+await writeFile("todos.json",JSON.stringify(todos,null,2));
+return Response.json(editedTodo);
+}
