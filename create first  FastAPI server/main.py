76c5d99d6cdea77ebe2d085ce6b76276bbdb377d
sharmaash730-ops <sharmaash730@gmail.com>
2026-05-11 00:05:11 -0700

    Update main.py

--- create first  FastAPI server/main.py
+++ create first  FastAPI server/main.py
@@ -1,1 +1,12 @@
 
+from fastapi import FastAPI
+
+app=FastAPI()
+
+@app.get("/")
+def.home():
+  return "welcome to FASTAPI server"
+
+@app.get("/contact")
+def.contact():
+  return("you can contact us any Time")
