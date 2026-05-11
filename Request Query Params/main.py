b2fd4b204cd86367a51dbd84c56078b1529e02c5
sharmaash730-ops <sharmaash730@gmail.com>
2026-05-11 03:38:41 -0700

    Update main.py

--- Request Query Params/main.py
+++ Request Query Params/main.py
@@ -1,1 +1,15 @@
 
+from fastapi import FastAPI,Request
+
+app=FastAPI()
+
+@app.get("/")
+def home():
+  return "Request query params"
+
+@app.get("/greet")
+def greet_user(request:Request):
+  query_params=dict(request.query_params)
+  return {
+    "greet":"hello {query_params.get("name")},your age is{query_params.get("age")}
+  }
