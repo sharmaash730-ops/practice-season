ac817264feca0c373c94e70347daf063aa3b985e
sharmaash730-ops <sharmaash730@gmail.com>
2026-05-11 02:30:38 -0700

    Update main.py

--- queryparams/main.py
+++ queryparams/main.py
@@ -1,1 +1,14 @@
 
+from fastapi import FastAPI
+
+app=FastAPI()
+
+@app.get("/home")
+def home():
+  return"welcome to our first FastAPi"
+
+@app.get("/greet")
+def greet_user(name:str ,age:int):
+  return {
+    "error":f"hello{name},Your age is {age}"
+  }
