514f2a21832dbfb59a4d4f26df989c51781c1802
sharmaash730-ops <sharmaash730@gmail.com>
2026-05-11 01:53:46 -0700

    Update main.py

--- dynamic path params/main.py
+++ dynamic path params/main.py
@@ -1,1 +1,28 @@
 
+from fastapi import FastAPI
+from mockData import products
+
+app=FastAPI()
+
+@app.get("/")
+def home():
+  return "welcome to First FastAPI"
+
+@app.get("/products")
+def get_products():
+  return products
+
+##dynamic params
+@app.get("/product/{product_id}")
+def get_one_Product(product_id:int):
+
+  #if product available with id,return product ,else return error message
+  for oneProduct in products:
+    if oneProduct.get("id")==product_id:
+      return oneProduct
+      return {
+        "error":"product not found for this id"
+      }
+
+
+  
