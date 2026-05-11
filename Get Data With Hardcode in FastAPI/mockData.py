e0da5ab4d959e7aad59435914b26ee6adfa69b05
sharmaash730-ops <sharmaash730@gmail.com>
2026-05-11 00:12:26 -0700

    Add mock product data to mockData.py

--- Get Data With Hardcode in FastAPI/mockData.py
+++ Get Data With Hardcode in FastAPI/mockData.py
@@ -1,1 +1,14 @@
 
+products=[
+   {
+        "id":1,
+        "title":"mobile phone",
+        "price":1000,
+        "count":10
+    },{
+        "id":2,
+        "title":"Apple Macbook",
+        "price":20000,
+        "count":12
+    }
+]
