f06ed6844bc03f62368d89c21bf150d371c2f26d
sharmaash730-ops <sharmaash730@gmail.com>
2026-05-11 02:31:50 -0700

    Update main.py

--- queryparams/main.py
+++ queryparams/main.py
@@ -1,11 +1,16 @@
 
 from fastapi import FastAPI
+from mockData import products
 
 app=FastAPI()
 
 @app.get("/home")
 def home():
   return"welcome to our first FastAPi"
+
+@app.get("/products")
+def get_products():
+  return products
 
 @app.get("/greet")
 def greet_user(name:str ,age:int):
