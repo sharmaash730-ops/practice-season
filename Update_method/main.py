10eb7b1cd8060e6ec2196ce2ee45022237fd6c78
sharmaash730-ops <sharmaash730@gmail.com>
2026-05-12 07:12:38 -0700

    Update main.py

--- Update_method/main.py
+++ Update_method/main.py
@@ -1,1 +1,16 @@
 
+from fastapi import FastAPI
+from mockData import products
+from dtos import ProductDTO
+
+app=FastAPI()
+
+@app.put("/update_product/{product_id"}
+def update_product(product_data:productDTO,product_id:int)
+
+  for index,oneProduct in enumerate(products):
+    if oneproduct.get("id")==product_id:
+      products[index]=product_data.model_dump()
+      return{"status":"product updated sucessfully..","products"product_data}
+
+return{"error":"Product not found for this ID"}
