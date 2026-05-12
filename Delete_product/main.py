21984f1b92dcf40adc9703f78ce0a1d9b19776c4
sharmaash730-ops <sharmaash730@gmail.com>
2026-05-12 07:31:05 -0700

    Update main.py

--- Delete_product/main.py
+++ Delete_product/main.py
@@ -1,1 +1,15 @@
 
+from fastapi import FastAPI
+from mockData import products
+from dtos import ProductDTO
+
+app=FastAPI()
+
+@app.delete("/delete_product/{product_id}")
+def delete_product(product_id:int):
+  for index,oneProduct in enumerate(products):
+    if oneProduct.get("id")==product_id:
+      delete_product=products.pop(index)
+      return{"status":"Product Deleted Successfully...","product"deleted_product}
+
+return{"error":"Product not found of this id"}
