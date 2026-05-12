45dd23a13492e95bb412e95e541343f9c5a803ec
sharmaash730-ops <sharmaash730@gmail.com>
2026-05-12 06:57:21 -0700

    Update main.py

--- post method/main.py
+++ post method/main.py
@@ -1,1 +1,12 @@
 
+from fastapi import FastAPI
+from mockData import products
+from dtos import productDTO
+
+app=FastAPI()
+
+@app.post("/create_post")
+def create_product(product_data:productDTO):
+  product_data=product_data.model_pump()
+  products.append(product_data)
+  return{"status":"Product Created Sucessfully...","data":products}
