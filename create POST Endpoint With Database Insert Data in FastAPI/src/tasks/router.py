bae7ea785c4703aa1049daf1b092f4dbdf2d90d9
sharmaash730-ops <sharmaash730@gmail.com>
2026-05-13 02:17:32 -0700

    Update router.py

--- create POST Endpoint With Database Insert Data in FastAPI/src/tasks/router.py
+++ create POST Endpoint With Database Insert Data in FastAPI/src/tasks/router.py
@@ -1,1 +1,11 @@
 
+from fastapi import APIRouter,depends
+from src.tasks import controller
+from src.tasks.dtos import TaskSchema
+from src.utils.db import get_db
+
+task routes=APIRouter(prefix="/tasks")
+
+@task_routes.post("/create")
+def create_task(body:TaskSchema,db = Depends(get_db)):
+    return controller.create_task(body,db)
