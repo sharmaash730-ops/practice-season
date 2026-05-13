290175f6116e977bd4884d5d81e046542c0f8844
sharmaash730-ops <sharmaash730@gmail.com>
2026-05-13 06:54:43 -0700

    Remove create and get one task functions

--- create GET Endpoint with Database with PostgresSQL Fetch Data in FastAPI/src/tasks/controller.py
+++ create GET Endpoint with Database with PostgresSQL Fetch Data in FastAPI/src/tasks/controller.py
@@ -4,35 +4,8 @@ from src.tasks.models import TaskModel
 from fastapi import HTTPException
 
 
-def create_task(body:TaskSchema,db:Session):
-    data = body.model_dump()
-    new_task = TaskModel(title = data["title"], 
-                         description = data["description"],
-                         is_completed = data["is_completed"]
-                         )
-    
-    db.add(new_task)
-    db.commit()
-    db.refresh(new_task)
-
-    return {"status":"Task Created Successfully...","data":new_task}
-
-
-
 def get_tasks(db:Session):
     tasks = db.query(TaskModel).all()
     return {"status":"All Tasks","data":tasks}
 
 
-def get_one_task( task_id:int,db:Session):
-    one_task=db.query(TaskModel).get(task_id)
-    if not one_task:
-        raise HTTPException(404,detail="Task id is incorrect")    
-    
-
-    return {"status":"Task Fetched Successfully","data":one_task}  
-
-
-     db.refresh(one_task)
-
-     return {"status":"Task Updated Successfully","data":one_task}
