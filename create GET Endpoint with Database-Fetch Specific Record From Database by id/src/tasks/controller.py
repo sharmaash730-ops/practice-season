19c72cf2e48dc655e2b84751788c04b5b40eb1ac
sharmaash730-ops <sharmaash730@gmail.com>
2026-05-13 07:05:37 -0700

    Update controller.py

--- create GET Endpoint with Database-Fetch Specific Record From Database by id/src/tasks/controller.py
+++ create GET Endpoint with Database-Fetch Specific Record From Database by id/src/tasks/controller.py
@@ -1,1 +1,14 @@
 
+from src.tasks.dtos import TaskSchema
+from sqlalchemy.orm import Session
+from src.tasks.models import TaskModel
+from fastapi import HTTPException
+
+def get_one_task( task_id:int,db:Session):
+    one_task=db.query(TaskModel).get(task_id)
+    if not one_task:
+        raise HTTPException(404,detail="Task id is incorrect")    
+    
+
+    return {"status":"Task Fetched Successfully","data":one_task}  
+
