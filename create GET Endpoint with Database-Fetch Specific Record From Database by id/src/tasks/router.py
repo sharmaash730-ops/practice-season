dae455a9a65e2a023e9141485f2f0b5a1ae7a4fe
sharmaash730-ops <sharmaash730@gmail.com>
2026-05-13 07:06:52 -0700

    Add GET endpoint to fetch task by ID

--- create GET Endpoint with Database-Fetch Specific Record From Database by id/src/tasks/router.py
+++ create GET Endpoint with Database-Fetch Specific Record From Database by id/src/tasks/router.py
@@ -1,1 +1,12 @@
 
+from fastapi import APIRouter,Depends
+from src.tasks import controller
+from src.tasks.dtos import TaskSchema
+from src.utils.db import get_db
+
+task_routes = APIRouter(prefix="/tasks")
+
+@task_routes.get("/one_task/{task_id}")
+def get_one_task(task_id:int,db=Depends(get_db)):
+    return controller.get_one_task(task_id,db)
+
