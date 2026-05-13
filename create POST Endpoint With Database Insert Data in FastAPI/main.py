582759e777b1a3d94bb38072d1a952d180a9453d
sharmaash730-ops <sharmaash730@gmail.com>
2026-05-13 02:12:16 -0700

    Update main.py

--- create POST Endpoint With Database Insert Data in FastAPI/main.py
+++ create POST Endpoint With Database Insert Data in FastAPI/main.py
@@ -1,1 +1,10 @@
 
+from fastapi import FastAPI
+from src.utils.db import Base,engine
+from src.tasks.models import TaskModel
+from src.tasks.router import task_router
+
+Base.metadata.create_all(engine)
+
+app=FastAPI(title="This is my task managment")
+app.include_router(task_routes)
