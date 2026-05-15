e98cec6a3694327490617cbdc8e18c4d2f4b1855
sharmaash730-ops <sharmaash730@gmail.com>
2026-05-15 07:24:48 -0700

    Update main.py

--- create UPDATE endpoint with database/main.py
+++ create UPDATE endpoint with database/main.py
@@ -1,1 +1,11 @@
 
+
+from fastapi import FastAPI
+from src.utils.db import Base,engine
+from src.tasks.models import TaskModel
+from src.tasks.router import task_routes
+
+Base.metadata.create_all(engine)
+
+app=FastAPI(title="This is my Task Managment Applications")
+app.include_router(task_routes)
