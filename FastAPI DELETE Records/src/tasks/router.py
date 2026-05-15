f0e009d37253d6f7ddfc662e178de05266379318
sharmaash730-ops <sharmaash730@gmail.com>
2026-05-15 07:43:02 -0700

    Refactor task routes to keep only delete_task
    Removed unused task route methods and retained only the delete_task method.

--- FastAPI DELETE Records/src/tasks/router.py
+++ FastAPI DELETE Records/src/tasks/router.py
@@ -5,24 +5,6 @@ from src.utils.db import get_db
 
 task_routes = APIRouter(prefix="/tasks")
 
-@task_routes.post("/create")
-def create_task(body:TaskSchema,db = Depends(get_db)):
-    return controller.create_task(body,db)
-
-
-@task_routes.get("/all_tasks")
-def get_all_tasks(db = Depends(get_db)):
-    return controller.get_tasks(db)
-
-
-@task_routes.get("/one_task/{task_id}")
-def get_one_task(task_id:int,db=Depends(get_db)):
-    return controller.get_one_task(task_id,db)
-
-@task_routes.put("/update_task/{task_id}")
-def update_task(body:TaskSchema,task_id:int,db = Depends(get_db)):
-    return controller.update_task(body,task_id,db)
-
 @task_routes.delete("/delete_task/{task_id}")
 def delete_task(task_id:int,db = Depends(get_db)):
     return controller.delete_task(task_id,db)
