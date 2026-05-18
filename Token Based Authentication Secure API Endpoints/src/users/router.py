e65fb20491089c2c93788c3bcb04a36489d60994
sharmaash730-ops <sharmaash730@gmail.com>
2026-05-18 09:16:29 -0700

    Add user registration and login endpoints

--- Token Based Authentication Secure API Endpoints/src/users/router.py
+++ Token Based Authentication Secure API Endpoints/src/users/router.py
@@ -1,1 +1,17 @@
+from fastapi import APIRouter,Depends,status
+from sqlalchemy.orm import Session
+from src.utils.db import get_db
+from src.users.dtos import UserSchema,UserResponseSchema
+from src.user import controller
 
+user_routes = APIRouter(prefix="/user")
+
+
+@user_routes.post("/register",response_model=UserResponseSchema,status_code=status.HTTP_201_CREATED)
+def register(body:UserSchema,db=Session=Depends(get_db)):
+  return controller.register(body,db)
+
+
+@user_routes.post("/login",status_code=status.HTTP_200_OK)
+def login(body:LoginSchema,db:Session=Depends(get_db)):
+    return controller.login_user(body,db)
