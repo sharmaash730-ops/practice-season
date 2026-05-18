6d4266832a1da7cb1da7b798f46d066c34ff3afc
sharmaash730-ops <sharmaash730@gmail.com>
2026-05-18 08:09:17 -0700

    Update controller.py

--- User Registration Advanced Validation &Error Handling /src/tasks/controller.py
+++ User Registration Advanced Validation &Error Handling /src/tasks/controller.py
@@ -1,6 +1,15 @@
+from fastapi import HTTPException
 from src.user.dtos import UserSchema,LoginSchema
 from sqlalchemy.orm import Session
+from src.user.models import UserModel
 
 def register(body:UserSchema, db:Session):
-  print(body)
+  is_user = db.query(UserModel).filter(UserModel.username == body.username).first()
+  if is_user:
+    raise HTTPException(400,detail="Username already Exist..")
+
+  is_user = db.query(UserModel).filter(UserModel.email == body.email).first()
+  if is_user:
+    raise HTTPException(400,detail="Email address already Exist..")
+    
   return{"msg":"Registration Done"}
