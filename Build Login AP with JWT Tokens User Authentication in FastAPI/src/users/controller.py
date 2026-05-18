04e1819a33b63a0e2fafcdc7e8ad01a98bdfdbf9
sharmaash730-ops <sharmaash730@gmail.com>
2026-05-18 08:49:00 -0700

    Update controller.py

--- Build Login AP with JWT Tokens User Authentication in FastAPI/src/users/controller.py
+++ Build Login AP with JWT Tokens User Authentication in FastAPI/src/users/controller.py
@@ -1,1 +1,37 @@
+from fastapi import HTTPException
+from src.user.dtos import UserSchema,LoginSchema
+from sqlalchemy.orm import Session
+from src.user.models import UserModel
+#password Hashing
+from pwdlib import PasswordHash
 
+password_hash = PasswordHash.recommended()
+
+def get_password_hash(password):
+    return password_hash.hash(password)
+
+def register(body:UserSchema, db:Session):
+  is_user = db.query(UserModel).filter(UserModel.username == body.username).first()
+  if is_user:
+    raise HTTPException(400,detail="Username already Exist..")
+
+  is_user = db.query(UserModel).filter(UserModel.email == body.email).first()
+  if is_user:
+    raise HTTPException(400,detail="Email address already Exist..")
+
+  hash_password = get_password_hash(body.password)
+
+    new_user=UserModel(
+        name = body.name,
+        username = body.username,
+        hash_password = hash_password, 
+        email = body.email
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
+    
+  return{"msg":"Registration Done"}
