3e1a0c43481ad5af4bca30d394f21f8ef82e1990
sharmaash730-ops <sharmaash730@gmail.com>
2026-05-18 08:15:38 -0700

    Fix response model in user registration route

--- User Registration Advanced Validation &Error Handling /src/tasks/router.py
+++ User Registration Advanced Validation &Error Handling /src/tasks/router.py
@@ -7,6 +7,6 @@ from src.user import controller
 user_routes = APIRouter(prefix="/user")
 
 
-@user_routes.post("/register",status_code=status.HTTP_201_CREATED)
+@user_routes.post("/register",response_model=status_code=status.HTTP_201_CREATED)
 def register(body:UserSchema,db=Session=Depends(get_db)):
   return controller.register(body,db)
