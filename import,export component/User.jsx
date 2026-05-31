723b987fc2ec9b1b8854795687bdc1a434281fde
sharmaash730-ops <sharmaash730@gmail.com>
2026-05-31 06:58:53 -0700

    Create User component with profile export

---
+++ import,export component/User.jsx
@@ -0,0 +1,18 @@
+function User(){
+  return(
+    <div>
+      <h1>User page</h1>
+      <p>this is our User page</p>
+    </div>
+  )
+}
+export default User;
+
+export function Profle(){
+  return(
+    <div>
+      <h1>profile page</h1>
+      <p>this is our profile page</p>
+    </div>
+  )
+}
