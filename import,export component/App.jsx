448e67dca37997763777fe5ea06c3321d0825a48
sharmaash730-ops <sharmaash730@gmail.com>
2026-05-31 06:56:01 -0700

    Add App component with User imports

---
+++ import,export component/App.jsx
@@ -0,0 +1,11 @@
+import User from "./User"
+import {Prifile} from "./User"
+const App = () =>{
+  return(
+    <div>
+      <h1>App component</h1>
+      <p>this is my first page</p>
+    </div>
+    )
+ }
+export default App;
