e80c4c47f78077185476a5193577bf8df71fed70
sharmaash730-ops <sharmaash730@gmail.com>
2026-05-31 05:55:33 -0700

    Create App component with greeting and User component

---
+++ first_component/App.jsx
@@ -0,0 +1,12 @@
+import User from "./User"
+
+const App = () => {
+  return(
+    <div>
+      <h1>Hello world</h1>
+      <p>this is my first app</p>
+      <User />
+    </div>
+  )
+}
+export default App
